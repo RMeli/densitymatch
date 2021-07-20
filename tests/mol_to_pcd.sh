927b0b66b9de4feac6fea22d480e5727836e7fea
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-07-20 16:30:18 +0100

    BRD4 tests

--- tests/mol_to_pcd.sh
+++ tests/mol_to_pcd.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+python transform.py ligands/BRD4/ligand-*.sdf
+python transform.py ligands/CDK2/*.sdf
 
 for ligand in "BRD4" "CDK2"
 do
-    for sdf in $(ls ligands/${ligand}/*.sdf)
+    for sdf in $(ls ligands/${ligand}/*_tran.sdf)
     do
         fout="${sdf%.*}.pcd"
 
-        singularity run --nv --app python ../containers/densitymatch.sif \
+        singularity run --nv --app python ../development/densitymatch.sif \
             ../molgrid_to_pcd.py ${sdf} -m ../files/ligmap -r 0.5 -o ${fout} -v
     done
 done

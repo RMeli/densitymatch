18245e87895bf3f2b65192f636f37016c394fcab
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-08-20 10:29:47 +0100

    documentation and extract transformation in standalone function

--- tests/utils.py
+++ tests/utils.py
@@ -15,6 +15,42 @@ import py3Dmol
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+def transform_and_add_conformer(mol, tran, fromConfId=0, toConfId=1) -> None:
+    """
+    Apply affine transformation to molecule.
+
+    Parameters
+    ----------
+    mol:
+        RDKit molecule
+    tran:
+        Affine transformation matrix (from Open3D)
+    fromConfId:
+        Conformer of mol to be transformed (original coordinates)
+    toConfId:
+        Conformer ID for the transformed conformer (transformed coordinates)
+    """
+    # Get coordinates to be transformed from conformer
+    coords = mol.GetConformer(fromConfId).GetPositions()
+
+    # Augment coordinates with ones
+    coords_aug = np.ones((coords.shape[0], 4))
+    coords_aug[:,:3] = coords
+
+    # Compute new (transformed) coordinates
+    coords_new = np.matmul(tran, coords_aug.T)[:3,:].T
+
+    # Add new coordinates as conformer
+    # Set new conformer index to toConfId
+    n_atoms = mol.GetNumAtoms()
+    conf = Chem.Conformer(n_atoms)
+    conf.SetId(toConfId)
+    for i in range(n_atoms):
+        conf.SetAtomPosition(i, coords_new[i,:])
+
+    # Conformer ID manually assigned above, avoid automatic assignement here
+    _ = mol.AddConformer(conf, assignId=False)
+
 class AlignShow:
     """
     Align molecules pairs within a list.
@@ -25,6 +61,13 @@ class AlignShow:
     def __init__(self, mols, pcds):
         """
         Store a series of RDKit molecules and corresponding point clouds.
+
+        Parameters
+        ----------
+        mols:
+            List of RDKit molecule
+        pcds:
+            List of Open3D point clouds (corresponding to mols)
         """
         assert len(mols) == len(pcds)
 
@@ -45,6 +88,13 @@ class AlignShow:
         """
         Align two molecules based on their PCD representation.
         Uses fit_and_score.
+
+        Parameters
+        ----------
+        idx1:
+            Index of molecule to align
+        idx2:
+            Index of molecule to which molecule idx1 is being aligned to
         """
         assert 0 <= idx1 < self.n
         assert 0 <= idx2 < self.n
@@ -58,28 +108,10 @@ class AlignShow:
     
         fit, cfit, tran = fit_and_score((pcd1, pcd2), voxel_size=0.5, threshold=0.5)
 
-        # Get coordinates to transform
-        # Conformer index corresponds to the molecule index
-        # This has been modified in the constructor
-        coords = mol1.GetConformer(idx1).GetPositions()
-
-        # Augment coordinates with ones
-        coords_aug = np.ones((coords.shape[0], 4))
-        coords_aug[:,:3] = coords
-
-        # Compute new (transformed) coordinates
-        coords_new = np.matmul(tran, coords_aug.T)[:3,:].T
-
-        # Add new coordinates as conformer
-        # Set conformer index to the index of the molecule it is being aligned to
-        n_atoms = mol1.GetNumAtoms()
-        conf = Chem.Conformer(n_atoms)
-        conf.SetId(idx2) # mols[idx1] is aligned to mols[idx2]
-        for i in range(n_atoms):
-            conf.SetAtomPosition(i, coords_new[i,:])
-
-        # Conformer ID manually assigned above, avoid automatic assignement here
-        _ = mol1.AddConformer(conf, assignId=False)
+        # Transform coordinates of mol1 (conformer idx1)
+        # Conformer index corresponds to the molecule index (modified in constructor)
+        # Store new coordinates as conformer with idx2
+        transform_and_add_conformer(mol1, tran, fromConfId=idx1, toConfId=idx2)
 
         # Cache scores
         self.scores[(idx1, idx2)] = cfit.fitness
@@ -89,6 +121,13 @@ class AlignShow:
     def show(self, idx1, idx2):
         """
         Show original molecules and aligned structures.
+
+        Parameters
+        ----------
+        idx1:
+            Index of molecule 1 (show original coordinates and transformed coordinates)
+        idx2:
+            Index of molecule 2 (show original coordinates)
         """
         assert 0 <= idx1 < self.n
         assert 0 <= idx2 < self.n
@@ -103,9 +142,9 @@ class AlignShow:
 
         # Select correct conformers
         # mols[idx1] is aligned to mols[idx1]
-        p.addModel(Chem.MolToMolBlock(mol1, confId=idx1),'sdf')
+        p.addModel(Chem.MolToMolBlock(mol1, confId=idx1),'sdf') # mol1 original coordinates
         p.addModel(Chem.MolToMolBlock(mol1, confId=idx2),'sdf') # mol1 aligned to mols[idx2]
-        p.addModel(Chem.MolToMolBlock(mol2, confId=idx2),'sdf')
+        p.addModel(Chem.MolToMolBlock(mol2, confId=idx2),'sdf') # mol2 original coordinates
 
         p.setStyle({"model": 0}, {'stick':{'colorscheme':'lightgreyCarbon'}})
         p.setStyle({"model": 1}, {'stick':{'colorscheme':'redCarbon'}})

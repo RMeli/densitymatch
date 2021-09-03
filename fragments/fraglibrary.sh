690d0ec4f5a3f17de483c69b645bfe903a978c65
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-09-03 13:55:10 +0100

    script to create conformers and point clouds

---
+++ fragments/fraglibrary.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Generate conformers and compute corresponding PCDs
+
+#python conformers.py DSIpoised.csv
+#python conformers.py VEHICLe.csv
+
+for frag in $(ls DSIpoised/*.sdf)
+do
+    echo ${frag}
+    outname=$(echo ${frag} | sed "s/.sdf/.pcd/g")
+    time singularity run --nv --app python ../development/densitymatch.sif \
+        ../molgrid_to_pcd.py ${frag} --ligmap ../files/ligmap -o ${outname}
+done

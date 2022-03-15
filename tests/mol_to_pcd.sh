50668ac4509296326c6b04ebcb17aacb4efe4f38
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-03-15 17:43:17 +0000

    improve mol to pcd script

--- tests/mol_to_pcd.sh
+++ tests/mol_to_pcd.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# ------------------------------------------------------------------------------
+# Pre-compute point clouds (PCD) from BRD4 and CDK2 inhibitors using libmolgrid-
+# generated densities.
+# This is done on translated molecules, so that the point cloud-based alignment
+# can be assessed.
+# ------------------------------------------------------------------------------
+
+# for ligand in "BRD4" "CDK2"
+# do
+#     for sdf in $(ls ligands/${ligand}/*.sdf)
+#     do
+#         fout="${sdf%.*}.pcd"
+
+#         singularity run --nv --app python ../development/densitymatch.sif \
+#             ../molgrid_to_pcd.py ${sdf} -m ../files/ligmap -r 0.5 -o ${fout} -v
+#     done
+# done
+
 python transform.py ligands/BRD4/ligand-*.sdf
 python transform.py ligands/CDK2/*.sdf
 

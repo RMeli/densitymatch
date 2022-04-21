77064e47f9fc51bf7f8308ae02b75435a4105200
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-04-21 18:42:58 +0100

    sync fragments

--- fragments/rsync.sh
+++ fragments/rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avzP htc-login:/data/biggin/lina3015/densitymatch/fragments/VEHICLe/ VEHICLe/
+rsync -avzP htc-login:/data/biggin/lina3015/densitymatch/fragments/VEHICLe-good/ VEHICLe-good/
